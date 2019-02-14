61ea9684b5b605795f0684be6f35fc4f403233cd
Grzegorz Hilgier <g.hilgier@gmail.com>
2019-02-14 19:04:01 +0100

    BetMenu split to header and cpp

--- 1/cAnimation.h
+++ 1/cAnimation.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "stdafx.h"
 //Implements movement animation of object BITMAP from start coords to stop coords
+//TODO change constructor parameters to vector instead of several integers
 class cAnimation:public cMenu
 {
 	int iteration;
